4873f2986a4c9c6460b4c86426b8a61a52b4a25a
Jose Alfredo <ja125garcia@gmail.com>
2026-01-16 16:47:23 -0600

    Add auth

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_20_203912) do
+ActiveRecord::Schema[8.0].define(version: 2026_01_16_224549) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -110,6 +110,15 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_203912) do
     t.index ["provider"], name: "index_models_on_provider"
   end
 
+  create_table "sessions", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.string "ip_address"
+    t.string "user_agent"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_sessions_on_user_id"
+  end
+
   create_table "tool_calls", force: :cascade do |t|
     t.string "tool_call_id", null: false
     t.string "name", null: false
@@ -122,6 +131,14 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_203912) do
     t.index ["tool_call_id"], name: "index_tool_calls_on_tool_call_id", unique: true
   end
 
+  create_table "users", force: :cascade do |t|
+    t.string "email_address", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email_address"], name: "index_users_on_email_address", unique: true
+  end
+
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "aeropress_recipes", "coffee_beans"
@@ -129,5 +146,6 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_20_203912) do
   add_foreign_key "messages", "chats"
   add_foreign_key "messages", "models"
   add_foreign_key "messages", "tool_calls"
+  add_foreign_key "sessions", "users"
   add_foreign_key "tool_calls", "messages"
 end
