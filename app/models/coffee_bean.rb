a98c8169f9787796781704e51ce5071c64d9549a
Jose Alfredo <ja125garcia@gmail.com>
2026-01-16 18:30:09 -0600

    Better views

--- app/models/coffee_bean.rb
+++ app/models/coffee_bean.rb
@@ -3,4 +3,8 @@ class CoffeeBean < ApplicationRecord
   has_many_attached :images
   has_many :aeropress_recipes, dependent: :destroy
   has_many :v60_recipes, dependent: :destroy
+
+  def display_name
+    brand.present? ? brand : "Coffee Bean ##{id}"
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,7 @@
 class User < ApplicationRecord
   has_secure_password
   has_many :sessions, dependent: :destroy
-  has_many :coffee_beans, dependent: :destroy
+  has_many :coffee_beans, -> { order(created_at: :desc) }, dependent: :destroy
 
   normalizes :email_address, with: ->(e) { e.strip.downcase }
 end
