11ddb266d94ee6e226bd839b51d54f43c41ae7a6
Jose Alfredo <ja125garcia@gmail.com>
2026-01-29 23:16:56 -0600

    Use class << self instead of self.method in brewing methods class

--- app/models/brewing_methods.rb
+++ app/models/brewing_methods.rb
@@ -7,23 +7,25 @@ module BrewingMethods
     "espresso" => BrewingMethods::Espresso
   }.freeze
 
-  def self.all
-    REGISTRY.keys
-  end
+  class << self
+    def all
+      REGISTRY.keys
+    end
 
-  def self.for(type)
-    REGISTRY[type] || raise(ArgumentError, "Unknown brewing method: #{type}")
-  end
+    def for(type)
+      REGISTRY[type] || raise(ArgumentError, "Unknown brewing method: #{type}")
+    end
 
-  def self.label(type)
-    self.for(type).label
-  end
+    def label(type)
+      self.for(type).label
+    end
 
-  def self.icon(type)
-    self.for(type).icon
-  end
+    def icon(type)
+      self.for(type).icon
+    end
 
-  def self.options_for_select
-    REGISTRY.map { |type, klass| [ klass.label, type ] }
+    def options_for_select
+      REGISTRY.map { |type, klass| [ klass.label, type ] }
+    end
   end
 end
